7ae105879d1092ccf8dc996eab1826b1b6e5162d
Mordical62645 <p.tecson.rizal@gmail.com>
2025-06-03 20:04:20 +0800

    map initialized

--- wad_data.py
+++ wad_data.py
@@ -3,6 +3,9 @@ from wad_reader import WADReader
 class WADData:
     def __init__(self, engine, map_name):
         self.reader = WADReader(engine.wad_path)
+        self.map_index = self.get_lump_index(lump_name=map_name)
+        print(f'\n{map_name}_index = {self.map_index}')
+
         self.reader.close()
 
     def get_lump_index(self, lump_name):
